212f42dde0d067057974e7ec4aa5b355fa38f480
grant <grant5014792@gmail.com>
2026-05-27 15:23:22 +0800

    fix: correct TSMC fab area regions

--- lib/branding.test.ts
+++ lib/branding.test.ts
@@ -9,6 +9,7 @@ describe("branding", () => {
 
   it("defines the active factory areas", () => {
     expect(ACTIVE_FACTORY_AREAS.map((area) => area.name)).toEqual([
+      "Fab 1",
       "Fab 2",
       "Fab 3",
       "Fab 5",
@@ -20,9 +21,28 @@ describe("branding", () => {
       "Fab 14",
       "Fab 18",
       "Fab 15",
-      "Fab 22 高雄",
-      "Fab 25 嘉義/台南沙崙",
+      "Fab 22",
+      "Fab 25",
     ]);
+  });
+
+  it("maps factories to their formal science park areas", () => {
+    expect(Object.fromEntries(ACTIVE_FACTORY_AREAS.map((area) => [area.name, area.city]))).toEqual({
+      "Fab 1": "新竹科學園區竹科園區工研院中興園區",
+      "Fab 2": "新竹科學園區竹科園區",
+      "Fab 3": "新竹科學園區竹科園區",
+      "Fab 5": "新竹科學園區竹科園區",
+      "Fab 6": "南部科學園區台南園區",
+      "Fab 8": "新竹科學園區竹科園區",
+      "Fab 12A": "新竹科學園區竹科園區",
+      "Fab 12B": "新竹科學園區竹科園區",
+      "Fab 20": "新竹科學園區竹科園區",
+      "Fab 14": "南部科學園區台南園區",
+      "Fab 18": "南部科學園區台南園區",
+      "Fab 15": "中部科學園區台中園區",
+      "Fab 22": "南部科學園區高雄楠梓園區",
+      "Fab 25": "中部科學園區台中園區",
+    });
   });
 
   it("defaults users to the Hsinchu factory", () => {
