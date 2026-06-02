ca49f0b593c75195b8bdfe12d200508c05dd2202
timsu92 <33785401+timsu92@users.noreply.github.com>
2026-06-02 19:01:02 +0000

    ci(test): show test results in GitHub Actions views

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI
+    ? [["github"], ["junit", { outputFile: "test-results/playwright-junit.xml" }]]
+    : "html",
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
